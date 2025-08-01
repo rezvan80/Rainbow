8eca87f7005abb66266b25502f7870df7c7f4dcd
rezvan80 <rbeheshtinezhad80@gmail.com>
2025-08-01 21:53:37 +0330

    Update test.py

--- test.py
+++ test.py
@@ -288,15 +288,17 @@ def test(args, T, dqn, val_mem, metrics, results_dir, evaluate=False):
   metrics['steps'].append(T)
   T_rewards, T_Qs = [], []
   for i in range(n_ev):
+    env.j=i
   # Test performance over several episodes
     done[i] = True
+    reward_sum[i] ,done[i] = 0 , False
+    state[i] , _ = env.reset
+    state[i] = torch.tensor(state[i] ,  dtype = torch.float32 , device = 'cpu'
   for _ in range(args.evaluation_episodes):
-    while True:
+    while all(done) == False:
       for i in range(n_ev):
-        if done[i]:
-          reward_sum[i], done[i] = 0, False
-          state[i], _  = env.reset()
-          state[i] = torch.tensor(state[i] ,  dtype=torch.float32, device='cpu')
+       if done[i] == False:
+
         action[i] = dqn.act_e_greedy(state[i])  # Choose an action ε-greedily
         state[i], reward[i], done[i] , _ , _ = env.step(action[i])  # Step
         state[i] = torch.tensor(state[i], dtype=torch.float32, device='cpu')
@@ -304,33 +306,34 @@ def test(args, T, dqn, val_mem, metrics, results_dir, evaluate=False):
         if args.render:
           env.render()
 
-        if done[i]:
-          T_rewards.append(reward_sum[i])
-          break
-    env.close()
+        
+       T_rewards.append(reward_sum[i])
+          
+        
 
-    # Test Q-values over validation memory
-    for state[i] in val_mem[i]:  # Iterate over valid states
-      T_Qs.append(dqn[i].evaluate_q(state[i]))
+        # Test Q-values over validation memory
+        for state[i] in val_mem[i]:  # Iterate over valid states
+          T_Qs.append(dqn[i].evaluate_q(state[i]))
 
-    avg_reward, avg_Q = sum(T_rewards) / len(T_rewards), sum(T_Qs) / len(T_Qs)
-    if not evaluate:
+  avg_reward, avg_Q = sum(T_rewards) / len(T_rewards), sum(T_Qs) / len(T_Qs)
+  if not evaluate:
       # Save model parameters if improved
       if avg_reward > metrics['best_avg_reward']:
-        metrics['best_avg_reward'] = avg_reward
-        dqn[i].save(results_dir)
+          metrics['best_avg_reward'] = avg_reward'
+          for i in range(n_ev):
+            dqn[i].save(results_dir)
 
-      # Append to results and save metrics
-      metrics['rewards'].append(T_rewards)
-      metrics['Qs'].append(T_Qs)
-      torch.save(metrics, os.path.join(results_dir, 'metrics.pth'))
+          # Append to results and save metrics
+          metrics['rewards'].append(T_rewards)
+          metrics['Qs'].append(T_Qs)
+          torch.save(metrics, os.path.join(results_dir, 'metrics.pth'))
+ 
+          # Plot
+          _plot_line(metrics['steps'], metrics['rewards'], 'Reward', path=results_dir)
+          _plot_line(metrics['steps'], metrics['Qs'], 'Q', path=results_dir)
 
-      # Plot
-      _plot_line(metrics['steps'], metrics['rewards'], 'Reward', path=results_dir)
-      _plot_line(metrics['steps'], metrics['Qs'], 'Q', path=results_dir)
-
-    # Return average reward and Q-value
-    return avg_reward, avg_Q
+  # Return average reward and Q-value
+  return avg_reward, avg_Q
 
 
 # Plots min, max and mean + standard deviation bars of a population over time
