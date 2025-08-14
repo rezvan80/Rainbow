222b739c5cdb836a100a14288238a4dfcccb1f68
rezvan80 <rbeheshtinezhad80@gmail.com>
2025-08-14 21:06:00 +0330

    Update agent.py

--- agent.py
+++ agent.py
@@ -11,7 +11,7 @@ from model import DQN
 
 class Agent():
   def __init__(self, args, env):
-    self.action_space = env.action_space()
+    self.action_space = env.action_space().n
     self.atoms = args.atoms
     self.Vmin = args.V_min
     self.Vmax = args.V_max
